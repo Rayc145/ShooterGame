a1b668bf08c5bf693ce33edf91b2cb606f7c24f3
Rayc145 <raychen200510@hotmail.com>
2022-03-23 09:27:34 -0400

    more changes

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # Import and initialize the pygame library
+from multiprocessing import reduction
+from ntpath import realpath
+from operator import truediv
+from pickle import FALSE
+from re import L
+from telnetlib import GA
+from textwrap import indent
 from turtle import width
+from pkg_resources import fixup_namespace_packages
 import pygame
 pygame.init()
-
+import json
 # Set up the drawing window
-WIDTH, HEIGHT = 800, 500
+WIDTH, HEIGHT = 700, 700
 SIZE = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Shooter")
@@ -16,56 +24,128 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 DIRT = (87, 58, 2)
-speed = 5
-charx, chary = 20, 300
-board = [
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [[],[],[],[],[],[],[],[],[],[]], 
-    [["Ground"],["Ground"],["Ground"],["Ground"],["Ground"],["Ground"],["Ground"],["Ground"],["Ground"],["Ground"]], 
-    [[],[],[],[],[],[],[],[],[],[]],
-]
-GRIDHEIGHT = HEIGHT/10
-GRIDWIDTH = WIDTH/10
+charx, chary = 100, 350
+speed, jumpSpeed = 0, 0
+inAir = False
+GRIDHEIGHT = HEIGHT/20
+GRIDWIDTH = WIDTH/20
+
 # Functions
 def drawGrid(color):
     for rows in range(0, HEIGHT, int(GRIDHEIGHT)):
         pygame.draw.line(screen, color, [0, rows], [WIDTH, rows])
     for columns in range(0, WIDTH, int(GRIDWIDTH)):
         pygame.draw.line(screen, color, [columns, 0], [columns, HEIGHT])
-        
-def drawBoard():
-    for rows in range(len(board)):
-        for columns in range(len(board[rows])):
-            if (board[rows][columns] == ["Ground"]):
-                pygame.draw.rect(screen, DIRT, [columns*int(GRIDWIDTH), rows*int(GRIDHEIGHT), int(GRIDWIDTH), int(GRIDHEIGHT)])
+
+class Player:
+    def __init__(self, x, y):
+        self.width = 35
+        self.height = 35
+        self.color = RED
+        self.jumpSpeed = 0
+        self.inAir = False
+        self.rect = pygame.draw.rect(screen, self.color, [x, y, self.width, self.height])
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, listOfTiles):
+        # Character movement
+        dy = 0
+        dx = 0
+        keys = pygame.key.get_pressed()
+        dx = 0
+        if keys[pygame.K_a]:
+            dx = -5
+        if keys[pygame.K_d]:
+            dx = 5
+        if (keys[pygame.K_SPACE] or keys[pygame.K_w]) and not self.inAir:
+            self.inAir = True
+            self.jumpSpeed = -15   
+        self.jumpSpeed += 1    
+        if self.jumpSpeed > 10:
+            self.jumpSpeed = 10
+            self.inAir = True
+        dy += self.jumpSpeed
+        # Character collision
+        for tile in listOfTiles:
+            if tile.colliderect(self.rect.x, self.rect.y+dy, self.width, self.height):
+                if self.jumpSpeed < 0:
+                    dy = tile.bottom - self.rect.top 
+                    self.jumpSpeed = 0
+                elif self.jumpSpeed >= 0:
+                    dy = tile.top - self.rect.bottom
+                    self.inAir = False
+            if tile.colliderect(self.rect.x+dx, self.rect.y, self.width, self.height):
+                dx = 0
+        self.rect.y += dy
+        self.rect.x += dx
+        pygame.draw.rect(screen, self.color, [self.rect.x, self.rect.y, self.width, self.height])
+
+class Game:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        with open("levels.json", "r") as readFile:
+            data = json.load(readFile)
+            self.board = data["level1"]
+        """self.board = [
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],     
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],  
+            [1,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1], 
+            [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
+        ]"""
+        self.listOfTiles = []
+
+    def drawBoard(self):
+        for rows in range(len(self.board)):
+            for columns in range(len(self.board[rows])):
+                if (self.board[rows][columns] == 1):
+                    self.listOfTiles.append(pygame.draw.rect(screen, DIRT, [columns*int(self.width), rows*int(self.height), int(self.width), int(self.height)]))
+
+    def gameMaker(self, mousePos, button):
+        #1 - left, 2 - middle, 3 - right, 4 - scroll up, 5 - scroll down
+        (x, y) = mousePos
+        if button == 1:
+            self.board[int(y/int(self.height))][int(x/int(self.width))] = 1
+        elif button == 3:
+            self.board[int(y/int(self.height))][int(x/int(self.width))] = 0
+
     
-
-def drawChar(color, x, y):
-    pygame.draw.rect(screen, color, [x, y, 50, 50])
-
+gameBoard = Game(GRIDWIDTH, GRIDHEIGHT)
+character = Player(100, 350)
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            with open("levels.json", "r") as readFile:
+                data = json.load(readFile)
+            data["level1"] = gameBoard.board
+            with open("levels.json", "w") as writeFile:
+                json.dump(data, writeFile)
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            gameBoard.gameMaker(pygame.mouse.get_pos(), event.button)
     # Fill the background with white
     screen.fill(WHITE)
-    # Character movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        charx -= speed
-    if keys[pygame.K_d]:
-        charx += speed
     # Grid
     drawGrid(BLACK)
-    drawChar(RED, charx, chary)
-    drawBoard()
+    gameBoard.drawBoard()
+    character.update(gameBoard.listOfTiles)
     # Flip the display
     pygame.display.flip()
     clock.tick(30)
